d1e4d0ac046341373d459681da16cd585f81b1f4
gesha <genabro@ukr.net>
2022-10-01 00:35:11 +0300

    finished

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorka3
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine("Введіть першу сторону:");
+            rectangle.Side1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть другу сторону:");
+            rectangle.Side2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Периметр та Площа:");
+            Console.WriteLine($"Периметр:{rectangle.Perimiter}");
+            Console.WriteLine($"Площа:{rectangle.Area}");
+
+        }
+    }
+}
---
+++ ConsoleApp2/Rectangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorka3
+{
+    class Rectangle
+    {
+        private double side1, side2;
+        
+        public double Side1
+        {
+            set { side1 = value; }
+            get { return side1; } 
+        }
+        public double Side2
+        {
+            set { side2 = value; }
+            get { return side2 ; }
+        }
+
+
+        double area;
+      private  double AreaCalculator()
+        {
+            area = this.side1 * this.side2;    
+            return area;
+        }
+        public double Area
+        {
+            get { return AreaCalculator(); }
+        }
+
+
+        private  double perimiter;
+  private double PerimeterCalculator()
+        {
+            perimiter = (this.side1 + this.side2) * 2;
+            return perimiter;
+        }
+        public double Perimiter
+        {
+            get { return PerimeterCalculator(); }
+        }   
+    }
+}
